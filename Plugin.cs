4ee1ca047a42e53890829931d91a0ce563820595
Khilseith <78451857+Khilseith@users.noreply.github.com>
2023-02-08 12:26:43 -0800

    Move to using TCDRP instead of discord-rpc

--- Plugin.cs
+++ Plugin.cs
@@ -1,101 +1,55 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 using BepInEx.Logging;
-using System;
 using UnityEngine.SceneManagement;
 
 namespace TromboneRP;
 
-[BepInPlugin("Khilseith.TromboneRP", "Trombone Champ Rich Presence", "1.0.0")]
+[BepInDependency("TCDRP")]
+[BepInPlugin("Khilseith.TromboneRP", "Trombone Champ Rich Presence", "1.1.0")]
 public class TromboneRP : BaseUnityPlugin
 {
-
-    public static TromboneRP Instance;
-    internal static DiscordRPC.RichPresence prsnc;
     public static ManualLogSource Log;
+    private static readonly long DiscordRPClientID = 1070122620172898367;
 
 
     void Awake()
     {
         Log = Logger;
-        Logger.LogInfo("Loaded Trombone Champ Rich Presence 1.0.0.");
+        Logger.LogInfo("Loaded Trombone Champ Rich Presence 1.1.0.");
 
-        DiscordRPC.EventHandlers eventHandlers = default;
-        eventHandlers.readyCallback = (DiscordRPC.ReadyCallback)Delegate.Combine(eventHandlers.readyCallback, new DiscordRPC.ReadyCallback(ReadyCallback));
-        eventHandlers.disconnectedCallback = (DiscordRPC.DisconnectedCallback)Delegate.Combine(eventHandlers.disconnectedCallback, new DiscordRPC.DisconnectedCallback(DisconnectedCallback));
-        eventHandlers.errorCallback = (DiscordRPC.ErrorCallback)Delegate.Combine(eventHandlers.errorCallback, new DiscordRPC.ErrorCallback(ErrorCallback));
-
-        DiscordRPC.Initialize("1070122620172898367", ref eventHandlers, true, "0612");
-        prsnc = default;
-        SetStatus();
-        ReadyCallback();
+        TCDRP.API.InitRPC(DiscordRPClientID);
 
         SceneManager.sceneLoaded += OnScenceLoaded;
     }
 
-    private static void ErrorCallback(int errorCode, string message)
-    {
-        Log.LogError($"ErrorCallback: {errorCode}: {message}");
-    }
-    private static void DisconnectedCallback(int errorCode, string message)
-    {
-        Log.LogDebug($"DisconnectedCallback: {errorCode}: {message}");
-    }
-    private static void ReadyCallback()
-    {
-        Log.LogDebug("Working");
-    }
-
-    private void SetStatus()
-    {
-        prsnc.state = "Main Menu";
-        prsnc.details = null;
-        prsnc.startTimestamp = 0;
-        prsnc.largeImageKey = "game_icon";
-        prsnc.largeImageText = "Toot toot";
-        prsnc.smallImageKey = null;
-        prsnc.smallImageText = null;
-        prsnc.partyMax= 0;
-        prsnc.partySize = 0;
-        prsnc.endTimestamp = 0;
-        DiscordRPC.UpdatePresence(ref prsnc);
-    }
-
-    private void SetGameStatus(SingleTrackData trackData)
-    {
-        prsnc.state = "Playing a Song";
-        prsnc.details = trackData.trackname_long;
-        prsnc.endTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds() + trackData.length;
-        DiscordRPC.UpdatePresence(ref prsnc);
-    }
-
     private void OnScenceLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (mode == LoadSceneMode.Additive)
-            return;
 
         Logger.LogDebug($"Level {scene.buildIndex} ({scene.name}) was loaded");
 
+
         if (scene.name == GameplaySceneName)
         {
-            Logger.LogInfo("Gameplay scene loaded.");
-            _isInGameplay = true;
-            SetGameStatus(GlobalVariables.chosen_track_data);
-        }
-        else if (_isInGameplay)
-        {
-            Logger.LogInfo("Gameplay scene unloaded.");
-            _isInGameplay = false;
-            SetStatus();
-        }
+            TCDRP.API.SetActivity(
+                DiscordRPClientID,
+                state: "Playing a song",
+                detail: $"{GlobalVariables.chosen_track_data.artist} - {GlobalVariables.chosen_track_data.trackname_long}",
+                largeImage: "game_icon",
+                endTime: DateTimeOffset.Now.ToUnixTimeSeconds() + GlobalVariables.chosen_track_data.length
+                );
 
-        if (scene.name == TrackselectSceneName)
+        }
+        else if (scene.name == TrackselectSceneName)
         {
-            prsnc.state = "Selecting a Track";
-            DiscordRPC.UpdatePresence(ref prsnc);
+            TCDRP.API.SetActivity(DiscordRPClientID, state: "Selecting a track", largeImage: "game_icon");
+        }
+        else
+        {
+            TCDRP.API.SetActivity(DiscordRPClientID, state: "Browsing the menus", largeImage: "game_icon");
         }
     }
 
-    private static bool _isInGameplay;
     private const string GameplaySceneName = "gameplay";
     private const string TrackselectSceneName = "levelselect";
 }
